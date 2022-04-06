3f5616c2785a8335335cbeea81b914429ed2c2b2
Ckyiu <UnsignedArduino@outlook.com>
2022-04-05 21:22:45 -0400

    Add song

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
+function play_song (song: any[]) {
+    music.setVolume(20)
+    MusicalImages.init_musical_image()
+    MusicalImages.set_queue(song)
+    MusicalImages.play()
+}
 stats.turnStats(true)
+play_song([assets.animation`never_gonna_give_you_up_main`, assets.animation`never_gonna_give_you_up_accompaniment`])
